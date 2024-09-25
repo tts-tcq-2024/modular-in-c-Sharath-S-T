f4a9ce111b74fafe81d517d2c6d362028f5776e7
Sharath-S-T <sharath.st123@gmail.com>
2024-09-25 11:25:30 +0530

    Update color_tests.c

--- color_tests.c
+++ color_tests.c
@@ -3,7 +3,8 @@
 #include <assert.h>
 #include <stdio.h>
 
-void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor) {
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor)
+{
     ColorPair colorPair = GetColorFromPairNumber(pairNumber);
     char colorPairNames[16];
     ColorPairToString(&colorPair, colorPairNames);
@@ -12,7 +13,8 @@ void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorC
     assert(colorPair.minorColor == expectedMinor);
 }
 
-void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber) {
+void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber)
+{
     ColorPair colorPair = {major, minor};
     int pairNumber = GetPairNumberFromColor(&colorPair);
     printf("Got pair number %d for pair %s %s\n", pairNumber,
@@ -21,12 +23,9 @@ void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expected
 }
 
 void runTests() {
-    testNumberToPair(1, WHITE, BLUE);   // 1 -> White Blue
-    testNumberToPair(2, WHITE, ORANGE); // 2 -> White Orange
-    testNumberToPair(3, WHITE, GREEN);  // 3 -> White Green
-    testNumberToPair(4, WHITE, BROWN);   // 4 -> White Brown
-    testNumberToPair(5, WHITE, SLATE);   // 5 -> White Slate
-    testNumberToPair(6, RED, BLUE);      // 6 -> Red Blue
-    testPairToNumber(BLACK, ORANGE, 12); // Black Orange -> 12
-    testPairToNumber(VIOLET, SLATE, 25);  // Violet Slate -> 25
+    testNumberToPair(1, WHITE, BLUE);
+    testNumberToPair(5, WHITE, SLATE);
+    testNumberToPair(6, RED, BLUE);
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
 }
