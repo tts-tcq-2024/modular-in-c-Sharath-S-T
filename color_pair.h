f7db08bafe2ee51a93298430c4d81c754f2becca
Sharath-S-T <sharath.st123@gmail.com>
2024-09-25 10:44:06 +0530

    Update color_pair.h

--- color_pair.h
+++ color_pair.h
@@ -11,8 +11,8 @@ typedef struct {
     enum MinorColor minorColor;
 } ColorPair;
 
-const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
-const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
 ColorPair GetColorFromPairNumber(int pairNumber);
