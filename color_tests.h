6b4864f9f7e31d8d4505a84886eab63a329bc249
Sharath-S-T <sharath.st123@gmail.com>
2024-09-25 10:39:08 +0530

    Create color_tests.h

---
+++ color_tests.h
@@ -0,0 +1,10 @@
+#ifndef COLOR_TESTS_H
+#define COLOR_TESTS_H
+
+#include "color_pair.h"
+
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+void runTests();
+
+#endif // COLOR_TESTS_H
