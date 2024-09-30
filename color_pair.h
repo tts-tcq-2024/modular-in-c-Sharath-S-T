88eea5580499a9ca17918b8107aa1027554cc250
Sharath-S-T <sharath.st123@gmail.com>
2024-09-30 10:28:58 +0530

    Update color_pair.h

--- color_pair.h
+++ color_pair.h
@@ -7,6 +7,15 @@
 enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
 enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
 
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+const int INVALID_MAJOR_COLOR = -1;
+const int INVALID_MINOR_COLOR = -1;
+
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
