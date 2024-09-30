8ad50d37377ba04985c95ba85c60934ca969b1b2
Sharath-S-T <sharath.st123@gmail.com>
2024-09-30 10:23:11 +0530

    Update color_manual.h

--- color_manual.h
+++ color_manual.h
@@ -1,6 +1,9 @@
 #ifndef COLOR_MANUAL_H
 #define COLOR_MANUAL_H
 
+const int numberOfMajorColors = 5;
+const int numberOfMinorColors = 5;
+
 #include "color_pair.h"
 
 char* GenerateColorCodeManual();
