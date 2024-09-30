312d56126fc44b989070d53c7170db5636703be2
Sharath-S-T <sharath.st123@gmail.com>
2024-09-30 09:04:57 +0530

    Update color_tests.c

--- color_tests.c
+++ color_tests.c
@@ -2,21 +2,28 @@
 #include "color_tests.h"
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor)
-{
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor) {
     ColorPair colorPair = GetColorFromPairNumber(pairNumber);
-    char colorPairNames[16];
-    ColorPairToString(&colorPair, colorPairNames);
+    char* colorPairNames = ColorPairToString(&colorPair);
     assert(colorPair.majorColor == expectedMajor);
     assert(colorPair.minorColor == expectedMinor);
+    free(colorPairNames);  // Free the allocated memory
 }
 
-void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber)
-{
+void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber) {
     ColorPair colorPair = {major, minor};
     int pairNumber = GetPairNumberFromColor(&colorPair);
     assert(pairNumber == expectedPairNumber);
+}
+
+void testColorCodeManual() {
+    char* manual = GenerateColorCodeManual();
+    assert(strstr(manual, "1 -> White Blue") != NULL);
+    assert(strstr(manual, "25 -> Violet Slate") != NULL);
+    free(manual);  // Free the allocated memory for the manual
 }
 
 void runTests() {
@@ -25,4 +32,5 @@ void runTests() {
     testNumberToPair(6, RED, BLUE);
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
+    testColorCodeManual();
 }
