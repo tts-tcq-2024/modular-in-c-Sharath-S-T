3fa3089c1d675b3512a05c992ae1dc3a753a0914
Sharath-S-T <sharath.st123@gmail.com>
2024-09-30 10:44:23 +0530

    Update color_pair.c

--- color_pair.c
+++ color_pair.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+const int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+const int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
 int isValidMajorColor(int majorColor) {
     return majorColor >= 0 && majorColor < numberOfMajorColors;
 }
